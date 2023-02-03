affd32b368b07a64c57041bfc695737cd3154902
spiritupbro <rinoakbr@gmail.com>
2023-02-03 09:31:17 +0700

    already added when connecting to wallet and have pubkey

--- packages/apps/escrow-dashboard/src/components/Kvstore/KvstoreView.tsx
+++ packages/apps/escrow-dashboard/src/components/Kvstore/KvstoreView.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { MainPage } from './MainPage';
 import { AfterConnect } from './AfterConnect';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import { Dashboard } from './Dashboard';
+import { useAccount, useContractRead} from 'wagmi';
+import KVStore from "@human-protocol/core/abis/KVStore.json";
 export const KvstoreView = (): React.ReactElement => {
-  const { isConnected } = useAccount();
+  const { isConnected,address } = useAccount();
+  const { data } = useContractRead({
+      address: process.env.REACT_APP_CONTRACT as string,
+      abi: KVStore,
+      functionName: 'get',
+      args: [address, "public_key"],
+  })
+    console.log(data)
   return (
     <>
       {!isConnected && <MainPage />}
-      {isConnected && <AfterConnect />}
+    {isConnected && !data && <AfterConnect />}
+    {isConnected && data && <Dashboard />}
     </>
   );
 };
